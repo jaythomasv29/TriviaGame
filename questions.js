85c9b9c1c18e3246073107470fd246e4bd6a7aec
James Vongampai <jaythomasv29@gmail.com>
2019-02-06 23:13:39 -0800

    logic with trivia game pages and css

---
+++ questions.js
@@ -0,0 +1,74 @@
+var questions = [
+    {
+    "question": "Where are the cars of the brand Ferrari manufactured??",
+    "option1": "Romania",
+    "option2": "Russia",
+    "option3": "Germany",
+    "option4": "Italy",
+    "answer": "4"
+}, {
+    "question": "Which of the following car manufacturers had a war named after it?",
+    "option1": "Ford",
+    "option2": "Toyota",
+    "option3": "Honda",
+    "option4": "Subaru",
+    "answer": "2"
+}, {
+    "question": "Which of the following collision avoidance systems helps airplanes avoid colliding with each other?",
+    "option1": "TCAS",
+    "option2": "GPWS ",
+    "option3": "OCAS",
+    "option4": "ONIC",
+    "answer": "1"
+}, {
+    "question": "Which of these companies does NOT manufacture motorcycles?",
+    "option1": "Honda",
+    "option2": "Kawasaki",
+    "option3": "Yamaha",
+    "option4": "Toyota",
+    "answer": "4"
+}, {
+    "question": "Automobiles produced by Telsa Motors operate on which form of energy?",
+    "option1": "Corn",
+    "option2": "Electricity",
+    "option3": "Nuclear",
+    "option4": "Steam",
+    "answer": "2"
+}, {
+    "question": "Which car tire manufacturer is famous for its &quot;Eagle&quot; brand of tires, and is the official tire supplier of NASCAR?",
+    "option1": "Pirelli",
+    "option2": "Bridgestone",
+    "option3": "Goodyear",
+    "option4": "Yokohama",
+    "answer": "3"
+}, {
+    "question": "Lexus is a luxury car manufactuer owned by what major automotive brand to target the American Luxury car segment?",
+    "option1": "Toyota",
+    "option2": "Acura",
+    "option3": "Mazda",
+    "option4": "Yamaha",
+    "answer": "1"
+}, {
+    "question": "What are the cylinder-like parts that pump up and down within the engine?",
+    "option1": "Springs",
+    "option2": "Rods",
+    "option3": "Muffler",
+    "option4": "Piston",
+    "answer": "4"
+}, {
+    "question": "What is the name of Nissan's most popular electric car?",
+    "option1": "Tree",
+    "option2": "Leaf",
+    "option3": "Root",
+    "option4": "i-Cell",
+    "answer": "2"
+}, {
+    "question": "The 'B' in BMW stands for what?",
+    "option1": "Bavarian",
+    "option2": "British",
+    "option3": "Brazil",
+    "option4": "Bionical",
+    "answer": "1"
+}
+];
+console.log(questions);
